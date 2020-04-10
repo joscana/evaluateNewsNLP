b1d72469db557c44f60dc31797f020188047f8a7
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-04-10 15:32:28 -0400

    Fixed import error

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,13 @@
-import { handleSubmit, fetchData } from './js/formHandler'
+import formHandler from './js/formHandler'
 import { validateInput } from './js/validateInput'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/main.scss'
+
+const handleSubmit = formHandler.handleSubmit
+const fetchData = formHandler.fetchData
 
 export {
     handleSubmit,
